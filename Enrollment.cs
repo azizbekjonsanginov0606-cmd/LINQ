134dfda8c9c7f07ab2c883f602f438f343745e45
4Z7_S4N9_1NV_XV <azizbekjonsanginov0606@gmail.com>
2026-04-01 16:27:17 +0500

    Add files via upload

---
+++ Enrollment.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Enrollment
+{
+    public int Id { get; set; }
+    public int StudentId { get; set; }
+    public int CourseId { get; set; }
+    public DateTime EnrollmentDate { get; set; }
+}
---
+++ Student.cs
@@ -0,0 +1,8 @@
+using System;
+
+public class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public DateTime DateOfBirth { get; set; }
+}
